21677f3fa5e026b5a0f5afc49fffd33704a20a1d
Nicolas Stohler <nicolas.stohler@varian.com>
2025-01-28 15:14:31 +0100

    initial demo unit test

---
+++ IndyPegSolver.Tests/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace IndyPegSolver.Tests
+{
+    public class UnitTest1
+    {
+        private readonly Demo _demo;
+
+        public UnitTest1()
+        {
+            _demo = new Demo();
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-1, -1, -2)]
+        [InlineData(100, 200, 300)]
+        public void Add_ShouldReturnCorrectSum(int a, int b, int expectedSum)
+        {
+            // Act
+            var result = _demo.Add(a, b);
+
+            // Assert
+            result.ShouldBe(expectedSum);
+        }
+
+        [Fact]
+        public void Add_ShouldThrowOverflowException_OnOverflow()
+        {
+            // Arrange
+            int maxInt = int.MaxValue;
+
+            // Act & Assert
+            Should.Throw<OverflowException>(() => _demo.Add(maxInt, 1));
+        }
+
+        [Fact]
+        public void Add_ShouldThrowOverflowException_OnUnderflow()
+        {
+            // Arrange
+            int minInt = int.MinValue;
+
+            // Act & Assert
+            Should.Throw<OverflowException>(() => _demo.Add(minInt, -1));
+        }
+    }
+}
---
+++ IndyPegSolver/Demo.cs
@@ -0,0 +1,13 @@
+namespace IndyPegSolver
+{
+    public class Demo
+    {
+        public int Add(int x, int y)
+        {
+            checked
+            {
+                return x + y;
+            }
+        }
+    }
+}
