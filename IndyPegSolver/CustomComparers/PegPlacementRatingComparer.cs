790a36878d4dd9eb5000c47ffeb48a8df20210ac
Nicolas Stohler <nicolas.stohler@varian.com>
2025-01-31 11:55:36 +0100

    commented out code: try to rate R-pegs higher than L-pegs with a bonus

--- IndyPegSolver/CustomComparers/PegPlacementRatingComparer.cs
+++ IndyPegSolver/CustomComparers/PegPlacementRatingComparer.cs
@@ -11,11 +11,13 @@ public class PegPlacementRatingComparer : IComparer<PegPlacementRating>
 
     public PegPlacementRatingComparer()
     {
-        _pegPlacementComparer = new PegPlacementPositionDirectionComparer();
+        _pegPlacementComparer = new PegPlacementPositionDirectionComparer();        
+        //_pegPlacementComparer = new PegPlacementDirectionPositionComparer();        
     }
 
     public int Compare(PegPlacementRating x, PegPlacementRating y)
     {
+        //// org
         int ratingComparison = y.Rating.CompareTo(x.Rating); // Sort by rating in descending order
         if (ratingComparison != 0)
         {
@@ -23,5 +25,18 @@ public class PegPlacementRatingComparer : IComparer<PegPlacementRating>
         }
 
         return _pegPlacementComparer.Compare(x.PegPlacement, y.PegPlacement);
+
+        //// TODO: still ok? needs testing?
+        //// let's give some bonus to R-pegs over L-pegs
+        //var bonusForRPegs = 2;
+        //var xRating = x.Rating + (x.PegPlacement.State == SlotState.Right ? bonusForRPegs : 0);
+        //var yRating = y.Rating + (y.PegPlacement.State == SlotState.Right ? bonusForRPegs : 0);
+        //int ratingComparison = yRating.CompareTo(xRating); // Sort by rating in descending order
+        //if (ratingComparison != 0)
+        //{
+        //    return ratingComparison;
+        //}
+
+        //return _pegPlacementComparer.Compare(x.PegPlacement, y.PegPlacement);
     }
 }
