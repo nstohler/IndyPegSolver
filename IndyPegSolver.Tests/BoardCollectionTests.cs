a787616d99682720e9076bcaecc76040d21b41dc
Nicolas Stohler <nstohler@gmail.com>
2025-02-06 14:33:26 +0100

    add unit tests for saving/loading board meta data

---
+++ IndyPegSolver.Tests/BoardCollectionTests.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Xunit;
+using Shouldly;
+
+namespace IndyPegSolver.Tests
+{
+    public class BoardCollectionTests
+    {
+        [Fact]
+        public void AddBoard_ShouldAddBoardToCollection()
+        {
+            // Arrange
+            var boardCollection = new BoardCollection();
+            var boardMetadata = new BoardMetadata
+            {
+                Name = "Test Board",
+                Description = "A test board",
+                ExamplePegPlacementSolutions = new List<string> { "0-0-L|1-1-R" },
+                BestSolutionPegCount = 2,
+                MinimumPegsToStart = 2,
+                Board = new char[,]
+                {
+                { 'O', 'O' },
+                { 'O', '-' }
+                }
+            };
+
+            // Act
+            boardCollection.AddBoard(boardMetadata);
+
+            // Assert
+            boardCollection.Boards.ShouldContain(boardMetadata);
+        }
+
+        [Fact]
+        public void LoadFromFile_ShouldLoadBoardsFromFile()
+        {
+            // Arrange
+            var filePath = "test_boards.json";
+            var boardMetadata = new BoardMetadata
+            {
+                Name = "Test Board",
+                Description = "A test board",
+                ExamplePegPlacementSolutions = new List<string> { "0-0-L|1-1-R" },
+                BestSolutionPegCount = 2,
+                MinimumPegsToStart = 2,
+                Board = new char[,]
+                {
+                { 'O', 'O' },
+                { 'O', '-' }
+                }
+            };
+            var boards = new List<BoardMetadata> { boardMetadata };
+            var json = JsonConvert.SerializeObject(boards, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(filePath, json);
+
+            var boardCollection = new BoardCollection();
+
+            // Act
+            boardCollection.LoadFromFile(filePath);
+
+            // Assert
+            boardCollection.Boards.ShouldContain(boardMetadata);
+
+            // Clean up
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public void SaveToFile_ShouldSaveBoardsToFile()
+        {
+            // Arrange
+            var filePath = "test_boards.json";
+            var boardCollection = new BoardCollection();
+            var boardMetadata = new BoardMetadata
+            {
+                Name = "Test Board",
+                Description = "A test board",
+                ExamplePegPlacementSolutions = new List<string> { "0-0-L|1-1-R" },
+                BestSolutionPegCount = 2,
+                MinimumPegsToStart = 2,
+                Board = new char[,]
+                {
+                { 'O', 'O' },
+                { 'O', '-' }
+                }
+            };
+            boardCollection.AddBoard(boardMetadata);
+
+            // Act
+            boardCollection.SaveToFile(filePath);
+
+            // Assert
+            var json = File.ReadAllText(filePath);
+            var loadedBoards = JsonConvert.DeserializeObject<List<BoardMetadata>>(json);
+                        
+            loadedBoards.ShouldContain(boardMetadata);
+            loadedBoards.ShouldContain(boardMetadata);
+
+            // Clean up
+            File.Delete(filePath);
+        }
+    }
+}
--- IndyPegSolver/BoardMetaData.cs
+++ IndyPegSolver/BoardMetaData.cs
@@ -1,5 +1,5 @@
 ﻿
-public class BoardMetadata
+public class BoardMetadata : IEquatable<BoardMetadata>
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
@@ -10,4 +10,45 @@ public class BoardMetadata
     public int MinimumPegsToStart { get; set; }
     
     public char[,] Board { get; set; } = new char[0, 0];
+
+    public bool Equals(BoardMetadata other)
+    {
+        if (other == null) return false;
+
+        return Name == other.Name &&
+               Description == other.Description &&
+               ExamplePegPlacementSolutions.SequenceEqual(other.ExamplePegPlacementSolutions) &&
+               BestSolutionPegCount == other.BestSolutionPegCount &&
+               MinimumPegsToStart == other.MinimumPegsToStart &&
+               Board.Rank == other.Board.Rank &&
+               Enumerable.Range(0, Board.Rank).All(d => Board.GetLength(d) == other.Board.GetLength(d)) &&
+               Board.Cast<char>().SequenceEqual(other.Board.Cast<char>());
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is BoardMetadata other)
+        {
+            return Equals(other);
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        HashCode hash = new HashCode();
+        hash.Add(Name);
+        hash.Add(Description);
+        foreach (var solution in ExamplePegPlacementSolutions)
+        {
+            hash.Add(solution);
+        }
+        hash.Add(BestSolutionPegCount);
+        hash.Add(MinimumPegsToStart);
+        foreach (var value in Board)
+        {
+            hash.Add(value);
+        }
+        return hash.ToHashCode();
+    }    
 }
